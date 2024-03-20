d080eba12b6c84345d8ef6cb863faa79b6025f13
stephy <stephshiro35@gmail.com>
2024-03-21 02:04:52 +0300

    add

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // code your solution here
+
+
+function saturdayFun(activity = "roller-skate") {
+
+    return (`This Saturday, I want to ${activity}!`);
+}
+activity();
+activity("swim");
+
+
+ 
+const mondayWork = function(activity = "go to the office") {
+    console.log `This Monday, I will ${activity}.`;
+ };
+ 
+ mondayWork(); 
+ mondayWork("bathe my dog"); 
+
+function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+        return `You are ${flair}${adjective}${flair}!`;
+    };
+}
+
+
